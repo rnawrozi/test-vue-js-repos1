f37ec8da6866df2a2677e8d78c39a1d1c0fadf24
rnawrozi <121869482+rnawrozi@users.noreply.github.com>
2023-01-03 20:14:20 +0100

    Hier ist ein Hello World Vue js in 2.0er Version!
    Hier ist ein Hello World Vue js in 2.0er Version!

---
+++ Testapp01.vue
@@ -0,0 +1,19 @@
+<!--
+Say Hello World with Vue!
+-->
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1><center>{{ message }}<center></h1>
+  <h1><center>{{ message }}<center></h1>
+  <h1><center>{{ message }}<center></h1>
+</template><!--
